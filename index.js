4c52afdb8b40504d3700f578622e6accaa8dcccc
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-28 20:48:49 +0300

    added basic error handling

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const ejsMate = require("ejs-mate");
 
+const ExpressError = require("./helpers/expressError");
+
 const { router: website } = require("./router/website");
 
 app.set("view engine", "ejs");
@@ -21,6 +23,31 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 app.use("/campgrounds", website);
+
+// this catches all the errors and thanks to our custom errors handling class, we send responses accordingly
+app.use("/", (err, req, res, next) => {
+	const { message = "Something Went Wrong...", status = 500 } = err;
+	res.status(status).send(message);
+});
+
+// // error handler has to be at the end
+
+// app.use("/", (err, req, res, next) => {
+// 	console.log("=================ERROR=================");
+// 	console.log(err.message);
+// 	next(err);
+// 	- next(err); express handles it <=
+// 	- (if no error middleware after this one, express prints the error and the stack trace to the page, if there are any errors;		middlewares after this one, the control get passed to them, if they dont call next, we are left hanging, if they call next, 	its the same here)
+// 	- next(); next middlewares or to custom error handlers <=
+// 	- (if we dont have custom error handlers, express prints "can't get /PATH")
+// 	- only one error handler runs, the first from top to bottom
+// });
+
+// app.use("/", (err, req, res, next) => {
+// 	console.log("Second");
+// 	next();
+// 	- next(err);
+// });
 
 app.listen(5000, () => {
 	console.log("running at port 5000");
--- router/website.js
+++ router/website.js
@@ -3,38 +3,56 @@ const router = express.Router();
 
 const { getAll, getById, createNewCamp, updateCamp, deleteCamp } = require("../controllers/app");
 
-router.get("/", async (req, res) => {
-	const campgrounds = await getAll();
-	res.render("campgrounds/index", { campgrounds });
+const ExpressError = require("../helpers/expressError");
+
+router.get("/", async (req, res, next) => {
+	try {
+		const campgrounds = await getAll();
+		res.render("campgrounds/index", { campgrounds });
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.post("/", async (req, res) => {
-	console.log("fired");
-	const camp = req.body.campground;
-	console.log(camp);
-	const isCampgroundSaved = await createNewCamp({ ...camp });
-	if (isCampgroundSaved) res.redirect(`/campgrounds/${isCampgroundSaved._id}`);
-	else res.send("404");
+router.post("/", async (req, res, next) => {
+	try {
+		const camp = req.body.campground;
+		const isCampgroundSaved = await createNewCamp({ ...camp });
+		if (isCampgroundSaved) res.redirect(`/campgrounds/${isCampgroundSaved._id}`);
+	} catch (err) {
+		next(err);
+	}
 });
 
 router.get("/new", async (req, res) => {
 	res.render("campgrounds/new");
 });
 
-router.get("/:id", async (req, res) => {
-	const { id } = req.params;
-	const campground = await getById(id);
-	res.render("campgrounds/show", { campground });
+router.get("/:id", async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const campground = await getById(id);
+		res.render("campgrounds/show", { campground });
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get("/:id/edit", async (req, res) => {
-	const { id } = req.params;
-	const campground = await getById(id);
-	res.render("campgrounds/edit", { campground });
+router.get("/:id/edit", async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const campground = await getById(id);
+		if (campground) res.render("campgrounds/edit", { campground });
+
+		// old:
+		// else next(new ExpressError("Prodcut not found", "404"));
+		// up here we can see how throw ExpressError would work in an async function, just need to pass it in next() instead of throwing it on its own, this way, it will be caught by our app.use(err, req, res, next) error handler
+	} catch (err) {
+		next(err);
+	}
 });
 
 router.delete("/:id/delete", async (req, res) => {
-	console.log("delete route");
 	const { id } = req.params;
 	const campground = await deleteCamp(id);
 	if (campground) res.redirect("/campgrounds");
@@ -42,12 +60,15 @@ router.delete("/:id/delete", async (req, res) => {
 });
 
 router.patch("/:id/edit", async (req, res) => {
-	const camp = req.body.campground;
-	console.log("patch");
-	const { id } = req.params;
-	const isCampUpdated = await updateCamp(camp, id);
-	if (isCampUpdated) res.redirect(`/campgrounds/${id}`);
-	else res.redirect(`/campgrounds/${id}`);
+	try {
+		const camp = req.body.campground;
+		const { id } = req.params;
+		const isCampUpdated = await updateCamp(camp, id);
+		if (isCampUpdated) res.redirect(`/campgrounds/${id}`);
+		else res.redirect(`/campgrounds/${id}`);
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports.router = router;
